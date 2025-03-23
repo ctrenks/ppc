ed9c0c7552709d4411930262da7aaa0e630f609a
ctrenks <chris@trenkas.com>
2025-03-23 12:20:19 -0400

    type fix

--- app/playCasino/[slug]/route.ts
+++ app/playCasino/[slug]/route.ts
@@ -40,18 +40,15 @@ export async function GET(
     // console.log(casino);
     // Record the visit if we have a casino ID
     if (casino.id) {
-      const outbounding = null;
-      if (!outbounding) {
-        await prisma.outbounding.create({
-          data: {
-            userAgent: ua,
-            geo: visitorCountry,
-            ip: ip,
-            casinoId: casino.id,
-            referer: referer,
-          },soc
-        });
-      }
+      await prisma.outbounding.create({
+        data: {
+          userAgent: ua,
+          geo: visitorCountry,
+          ip: ip,
+          casinoId: casino.id,
+          referer: referer,
+        },
+      });
     }
 
     // Redirect to PPC URL if exists, otherwise casino URL
@@ -61,8 +58,11 @@ export async function GET(
       casino.ppc_links[0]?.url ?? casino.ppc?.[0]?.ppc_url ?? casino.url;
 
     if (redirectUrl) {
-      return NextResponse.redirect(new URL(redirectUrl), 302);
+      return NextResponse.redirect(redirectUrl, 302);
     }
+
+    // If we get here, we don't have a valid URL to redirect to
+    return NextResponse.redirect(new URL("/", request.url));
   } catch (error) {
     console.error("Error redirecting to casino:", error);
     return NextResponse.redirect(new URL("/error", request.url));
